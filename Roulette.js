27bd83b0b89eaaf55f11659409f1d04967f57fdc
taikoubou <godon.lykn.ows03@gmail.com>
2014-06-07 18:55:35 +0900

    テーブル表示にしてリストボックスを試作中

--- Roulette.js
+++ Roulette.js
@@ -1,18 +1,9 @@
 var main = function(){
 }
 
-var allButtonPositionSet = function(){
-	var Entrystrlen1 = document.getElementById("Entry1").innerHTML.length;
-	var Entrystrlen2 = document.getElementById("Entry2").innerHTML.length;
-	var Entrystrlen3 = document.getElementById("Entry3").innerHTML.length;
-
-	alert(Entrystrlen1+" "+Entrystrlen2+" "+Entrystrlen3);
-}
-
 var Entry1Button = function(){
 	var node = document.getElementById("Entry1");
 	node.innerHTML = "ほげほげ";
-	allButtonPositionSet();
 }
 
 var Entry2Button = function(){
